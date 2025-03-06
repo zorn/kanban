2a665d91e1784c2ffb2c8754475e64735705e80e
Mike Zornek <zorn@zornlabs.com>
2025-03-06 06:27:10 -0800

    Add failing test.

--- test/kanban_web/controllers/page_controller_test.exs
+++ test/kanban_web/controllers/page_controller_test.exs
@@ -2,7 +2,8 @@ defmodule KanbanWeb.PageControllerTest do
   use KanbanWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    # conn = get(conn, ~p"/")
+    # assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert false
   end
 end
