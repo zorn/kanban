71295fd0358140fb64dd92bf1c6f2a5042dbe35f
Mike Zornek <zorn@zornlabs.com>
2025-03-22 12:23:51 -0400

    Remove no longer needed `import` blocks.

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -4,21 +4,6 @@ module "swarm" {
 
 }
 
-# import {
-#   to = module.swarm.aws_key_pair.deployer_key
-#   id = "swarm-key"
-# }
-
 output "swarm_ssh_command" {
   value = module.swarm.ssh_command
 }
-
-# import {
-#   to = module.swarm.aws_instance.my_swarm
-#   id = "i-016a568dc54120133"
-# }
-
-# import {
-#   to = module.swarm.aws_security_group.swarm_sg
-#   id = "sg-0ad149e886bf00711"
-# }
